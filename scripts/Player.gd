43cf9979094be39315f46188bdf343d579efba45
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2023-02-26 00:36:36 +0100

    Add rotation for camera, add timeout for fire

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,11 +2,21 @@ extends KinematicBody2D
 
 signal fire
 
-const SPEED = 10000
-const ROTATION_SPEED = 1
-const TURNING_COEF = 0.5
-var map_size: Rect2
-onready var self_size: Rect2 = $Sprite.get_rect()
+const SPEED := 150
+const ROTATION_SPEED := 1.0
+const TURNING_COEF := 0.5
+var map_rect: Rect2
+var is_gun_ready := true
+
+onready var self_size: Vector2 = $Sprite.get_rect().size
+onready var fire_timeout = $FireTimeout
+
+export(float) var fire_timeout_sec
+
+
+func _ready() -> void:
+	fire_timeout.wait_time = fire_timeout_sec
+
 
 func handle_move(delta: float) -> void:
 	var velocity: Vector2 = Vector2()
@@ -26,36 +36,47 @@ func handle_move(delta: float) -> void:
 	if Input.is_action_pressed("move_down"):
 		velocity.y += current_speed
 
-	var half_size_x: float = self_size.size.x / 2
-	var half_size_y: float = self_size.size.y / 2
+	var half_size_x: float = self_size.x / 2
+	var half_size_y: float = self_size.y / 2
 
 	position.x = clamp(
 		position.x,
-		map_size.position.x + half_size_x,
-		map_size.end.x - half_size_x
+		map_rect.position.x + half_size_x,
+		map_rect.end.x - half_size_x
 	)
 	position.y = clamp(
 		position.y,
-		map_size.position.y + half_size_y,
-		map_size.end.y - half_size_y
+		map_rect.position.y + half_size_y,
+		map_rect.end.y - half_size_y
 	)
 
-	move_and_slide(velocity.rotated(rotation) * delta)
+	move_and_slide(velocity.rotated(rotation))
+
 
 func handle_fire() -> void: 
-	if Input.is_action_just_pressed("fire"):
-		var rotation_pos = Vector2.UP.rotated(rotation) * self_size.end.y
+	if Input.is_action_just_pressed("fire") and is_gun_ready:
+		var rotation_pos = Vector2.UP.rotated(rotation) * self_size.y / 2
+		is_gun_ready = false
+		fire_timeout.start()
+		
+		print(self_size.y)
 		emit_signal(
 			"fire",
 			rotation,
-			Vector2(position) + rotation_pos
+			position + rotation_pos
 		)
 
+
 func _process(delta: float) -> void:
-	if not map_size:
+	if not map_rect:
 		return
 	handle_move(delta)
 	handle_fire()
 
+
 func handle_map_size(map: Rect2) -> void:
-	map_size = map
+	map_rect = map
+
+
+func on_fire_timeout() -> void:
+	is_gun_ready = true
